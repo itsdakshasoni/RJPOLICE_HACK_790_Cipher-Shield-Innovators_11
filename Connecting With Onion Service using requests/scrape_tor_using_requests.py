047b8120a93bcd509b00a0857b6275346d5ef476
Abhijith <137030384+thisis-abhijith@users.noreply.github.com>
2024-01-17 18:14:40 +0530

    scrape_tor_using_requests.py

--- Connecting With Onion Service using requests/scrape_tor_using_requests.py
+++ Connecting With Onion Service using requests/scrape_tor_using_requests.py
@@ -5,8 +5,8 @@ def get_tor_session():
     session = requests.session()
     #45.112.57.110:6699
     # Tor uses the 9050 port as the default socks port
-    session.proxies = {'http':  'socks5://127.0.0.1:9150',
-                       'https': 'socks5://127.0.0.1:9150'}
+    session.proxies = {'http':  'socks5://127.0.0.1:9050',
+                       'https': 'socks5://127.0.0.1:9050'}
     return session
 
 # Make a request through the Tor connection
@@ -21,8 +21,8 @@ print(session.get("ednf5xiofeunsycu.onion/wiki-onion-urls.html").text)
 
 import requests
 session = requests.session()
-session.proxies = {'http':  'socks5h://localhost:9150',
-                   'https': 'socks5h://localhost:9150'}
+session.proxies = {'http':  'socks5h://localhost:9050',
+                   'https': 'socks5h://localhost:9050'}
 print(session.get('http://httpbin.org/ip').text) # prints {"origin": "67.205.146.164" }
 
 print(requests.get('http://httpbin.org/ip').text) # prints {"origin": "5.102.254.76" }
